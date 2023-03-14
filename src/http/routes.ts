cade88b202a617269bada8a5281cd2b038841c9e
luiszkm <luiszkm@gmail.com>
2023-03-14 09:59:08 -0300

    add controller

---
+++ src/http/routes.ts
@@ -0,0 +1,8 @@
+import { FastifyInstance } from "fastify";
+import { registerController } from "./controllers/register.controller";
+
+
+export async function appRoutes  (app:FastifyInstance) {
+  app.post('/users', registerController)
+
+}
---
+++ src/lib/prisma.ts
@@ -0,0 +1,7 @@
+import { env } from "../env"
+import { PrismaClient } from "@prisma/client";
+
+
+export const prisma = new PrismaClient({
+  log: env.NODE_ENV === 'dev' ? ['query'] : []
+})
