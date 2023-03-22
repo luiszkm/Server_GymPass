c5cbab5bb268dd95094a6467561d82ab77f00670
luiszkm <luiszkm@gmail.com>
2023-03-22 09:40:09 -0300

    add resresh token controller and test

--- src/http/controllers/users/routes.ts
+++ src/http/controllers/users/routes.ts
@@ -3,12 +3,13 @@ import { authenticateController } from "./authenticate.controller";
 import { verifyJWT } from "../../middlewares/verify-jwt";
 import { UserProfileController } from "./userProfile.controller";
 import { registerController } from "./register.controller";
+import { refreshController } from "./refresh.controller copy";
 
 export async function usersRoutes(app: FastifyInstance) {
   app.post('/users', registerController)
   app.post('/sessions', authenticateController)
 
-
+  app.patch('/token/refresh', refreshController)
   // «««««««« Authenticated »»»»»»»»» //
   app.get('/me', { onRequest: [verifyJWT] }, UserProfileController)
 }
